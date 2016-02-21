911e8d414a4601519e648b2bf1184497d683b3be
Bruno Filippone <bruno.filippone@b-fil.com>
2016-02-21 21:10:56 +0000

    Updated documentation

--- toml_config/sidebar-items.js
+++ toml_config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ConfigFactory","Implements helper functions for loading .toml files into a structure"]]});
+initSidebarItems({"struct":[["ConfigFactory","Implements helper functions for loading TOML files into a structureExamplesTo load a file into a Config struct, use `ConfigFactory`.Either `rustc_serialize` or `serde` can be used for serialization. Example using rustc_serializeExample using serde"]]});
